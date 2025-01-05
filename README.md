86f6e7d5e39b4e2078000248b4239944939b9c65
javadbat <javadbat@gmail.com>
2025-01-05 22:20:02 +0330

    add deprecation message

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jb-file-input-react
+
+> [!WARNING]  
+> this package is deprecated and moved to [jb-file-input/react](https://github.com/javadbat/jb-file-input/tree/main/react)
--- lib/JBFileInput.tsx
+++ lib/JBFileInput.tsx
@@ -21,42 +21,42 @@ export type JBFileInputEventType<T> = T & {
     target: JBFileInputWebComponent
 }
 const JBFileInput = forwardRef<JBFileInputWebComponent | null | undefined, JBFileInputProps>((props, ref) => {
-    const element = useRef<JBFileInputWebComponent | null>(null);
-    const [refChangeCount, refChangeCountSetter] = useState(0);
-    useImperativeHandle(
-        ref,
-        () => {
-            return (element ? element.current as JBFileInputWebComponent : {} as unknown as JBFileInputWebComponent);
-        },
-        [element],
-    );
-    useEffect(() => {
-        refChangeCountSetter(refChangeCount + 1);
-    }, [element.current]);
-    useEffect(() => {
-        if (element.current && props.acceptTypes) {
-            element.current.acceptTypes = props.acceptTypes;
-        }
-    }, [props.acceptTypes]);
-    useEffect(() => {
-        if (element.current && props.placeholderTitle) {
-            element.current.setAttribute('placeholder-title',props.placeholderTitle);
-        }
-    }, [props.placeholderTitle]);
-    useEffect(() => {
-        if (element.current && props.required) {
-            element.current.setAttribute('required', props.required?"true":"false");
-        }
-    }, [props.required]);
-    const onchange = useCallback((e: JBFileInputEventType<Event>) => {
-        if (props.onChange) {
-            props.onChange(e);
-        }
-    }, [props.onChange]);
-    useEvent(element.current, 'change', onchange, true);
-    return (
-        <jb-file-input ref={element} class={props.className}></jb-file-input>
-    );
+  const element = useRef<JBFileInputWebComponent | null>(null);
+  const [refChangeCount, refChangeCountSetter] = useState(0);
+  useImperativeHandle(
+    ref,
+    () => {
+      return (element ? element.current as JBFileInputWebComponent : {} as unknown as JBFileInputWebComponent);
+    },
+    [element],
+  );
+  useEffect(() => {
+    refChangeCountSetter(refChangeCount + 1);
+  }, [element.current]);
+  useEffect(() => {
+    if (element.current && props.acceptTypes) {
+      element.current.acceptTypes = props.acceptTypes;
+    }
+  }, [props.acceptTypes]);
+  useEffect(() => {
+    if (element.current && props.placeholderTitle) {
+      element.current.setAttribute('placeholder-title',props.placeholderTitle);
+    }
+  }, [props.placeholderTitle]);
+  useEffect(() => {
+    if (element.current && props.required) {
+      element.current.setAttribute('required', props.required?"true":"false");
+    }
+  }, [props.required]);
+  const onchange = useCallback((e: JBFileInputEventType<Event>) => {
+    if (props.onChange) {
+      props.onChange(e);
+    }
+  }, [props.onChange]);
+  useEvent(element.current, 'change', onchange, true);
+  return (
+    <jb-file-input ref={element} class={props.className}></jb-file-input>
+  );
 });
 JBFileInput.displayName = "JBFileInput";
 type JBFileInputProps = {
